c123a13c7cdd5a254998a79fcc4c6e64a1c2b797
A-Gui <sa6549872000@gmail.com>
2020-03-30 09:29:47 +0800

    update

--- pc.js
+++ pc.js
@@ -4,49 +4,49 @@ var titles = ["SideTitle1", "SideTitle2", "SideTitle3", "SideTitle4",
 
 // Topnav頁面切換
 function openWebtab(evt, webName) {
-var i, tabcontent, tablinks;
+	var i, tabcontent, tablinks;
 
-tabcontent = document.getElementsByClassName("tab-content");
-for (i = 0; i < tabcontent.length; i++) {
-	tabcontent[i].style.display = "none";
-}
-tablinks = document.getElementsByClassName("tab-links");
-for (i = 0; i < tablinks.length; i++) {
-	tablinks[i].className = tablinks[i].className.replace(" active", "");
-}
-document.getElementById(webName).style.display = "block";
-evt.currentTarget.className += " active";
+	tabcontent = document.getElementsByClassName("tab-content");
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tab-links");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(webName).style.display = "block";
+	evt.currentTarget.className += " active";
 
-// When the page refreshed, it reset variables.
-scrollTo(0, 0);
-// When the page refreshed, it stops playing. 
-stopVideo();
-closeNav();
+	// When the page refreshed, it reset variables.
+	scrollTo(0, 0);
+	// When the page refreshed, it stops playing. 
+	stopVideo();
+	closeNav();
 }
 // Sidenav頁面切換
 function openWebside(evt, webName) {
-var i, sidecontent, sidelinks;
+	var i, sidecontent, sidelinks;
 
-sidecontent = document.getElementsByClassName("sidenav-content");
-for (i = 0; i < sidecontent.length; i++) {
-	sidecontent[i].style.display = "none";
-}
-sidelinks = document.getElementsByClassName("sidenav-links");
-for (i = 0; i < sidelinks.length; i++) {
-	sidelinks[i].className = sidelinks[i].className.replace(" active", "");
-}
-document.getElementById(webName).style.display = "block";
-evt.currentTarget.className += " active";
+	sidecontent = document.getElementsByClassName("sidenav-content");
+	for (i = 0; i < sidecontent.length; i++) {
+		sidecontent[i].style.display = "none";
+	}
+	sidelinks = document.getElementsByClassName("sidenav-links");
+	for (i = 0; i < sidelinks.length; i++) {
+		sidelinks[i].className = sidelinks[i].className.replace(" active", "");
+	}
+	document.getElementById(webName).style.display = "block";
+	evt.currentTarget.className += " active";
 
-// When the page refreshed, it reset variables.
-scrollTo(0, 0);
-closeNav();
+	// When the page refreshed, it reset variables.
+	scrollTo(0, 0);
+	closeNav();
 }
 
 // This code loads the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
 
-tag.src = "https://www.youtube.com/iframe_api";
+tag.src = "https://www.youtube.com/player_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
